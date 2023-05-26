8b377b83321403f29c603f7189cb6f79fc405ea4
Sanechek1337 <rednaxela659@gmail.com>
2023-05-26 23:05:45 +0600

    it works O_O

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,41 @@ let freeCells = Array.from(gameCells); // Заполняем массив эле
 function toFillCell() {
 	const randomCellNumber = Math.floor(Math.random() * freeCells.length); // Выбор рандомного индекса из массива freeCells
 	freeCells[randomCellNumber].textContent = Math.round(Math.random()) ? '2' : '4'; // Заполняем выбранный элемент цифрой
-	freeCells.splice(randomCellNumber, 1) // Удаляем заполненный элемент из массива свободных клеток
+	freeCells.splice(randomCellNumber, 1); // Удаляем заполненный элемент из массива свободных клеток
 }
 
 function addClassesToCell() {
 	gameCells.map((cell) => {
 		const cellInnerNumber = Number(cell.textContent);
 		if (cellInnerNumber > 0) {
-			cell.className = `cell${cellInnerNumber}`
+			cell.className = `cell${cellInnerNumber}`;
 		} else {
-			cell.className = ''
+			cell.className = '';
 		}
 	})
 }
 
-toFillCell()
-toFillCell()
+toFillCell();
+toFillCell();
 addClassesToCell();
+
+let mergedBlocks = {}; // Объект нужен для определения складывался ли блок за текущий ход
 
 function handleKeyDown(event) {
 	const key = event.key;
 	let changed = false; // для проверки на изменения, чтобы не создавались новые блоки когда существующие уже некуда двигать
 
+
 	if (key === 'w' || key === 'ArrowUp') {
 
 		for (let j = 0; j < 4; j++) {
 			for (let i = 12; i < 16; i++) {
-
-				if (gameCells[i - 12].textContent !== '') {
+				if (gameCells[i - 12].textContent !== '' && !mergedBlocks[`${i - 12}`] && !mergedBlocks[`${i - 8}`]) {
+					// debugger
 					if (gameCells[i - 8].textContent === gameCells[i - 12].textContent) {
 						gameCells[i - 12].textContent = Number(gameCells[i - 8].textContent) * 2;
 						gameCells[i - 8].textContent = '';
+						mergedBlocks[`${i - 12}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i - 12].textContent === '' && gameCells[i - 8].textContent !== '') {
@@ -45,10 +49,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i - 8].textContent !== '') {
+				if (gameCells[i - 8].textContent !== '' && !mergedBlocks[`${i - 8}`] && !!mergedBlocks[`${i - 4}`]) {
 					if (gameCells[i - 4].textContent === gameCells[i - 8].textContent) {
 						gameCells[i - 8].textContent = Number(gameCells[i - 4].textContent) * 2;
 						gameCells[i - 4].textContent = '';
+						mergedBlocks[`${i - 8}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i - 8].textContent === '' && gameCells[i - 4].textContent !== '') {
@@ -57,10 +62,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i - 4].textContent !== '') {
+				if (gameCells[i - 4].textContent !== '' && !!mergedBlocks[`${i - 4}`]) {
 					if (gameCells[i].textContent === gameCells[i - 4].textContent) {
 						gameCells[i - 4].textContent = Number(gameCells[i].textContent) * 2;
 						gameCells[i].textContent = '';
+						mergedBlocks[`${i - 4}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i - 4].textContent === '' && gameCells[i].textContent !== '') {
@@ -68,7 +74,6 @@ function handleKeyDown(event) {
 					gameCells[i].textContent = '';
 					changed = true;
 				}
-
 			}
 		}
 		if (changed) {
@@ -79,15 +84,16 @@ function handleKeyDown(event) {
 
 
 	if (key === 's' || key === 'ArrowDown') {
-		// debugger
+		debugger
 		for (let j = 0; j < 4; j++) {
 
 			for (let i = 0; i < 4; i++) {
 				// debugger
-				if (gameCells[i + 12].textContent !== '') {
+				if (gameCells[i + 12].textContent !== '' && !mergedBlocks[`${i + 12}`] && !mergedBlocks[`${i + 8}`]) {
 					if (gameCells[i + 8].textContent === gameCells[i + 12].textContent) {
 						gameCells[i + 12].textContent = Number(gameCells[i + 8].textContent) * 2;
 						gameCells[i + 8].textContent = '';
+						mergedBlocks[`${i + 12}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 12].textContent === '' && gameCells[i + 8].textContent !== '') {
@@ -96,10 +102,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i + 8].textContent !== '') {
+				if (gameCells[i + 8].textContent !== '' && !mergedBlocks[`${i + 8}`] && !mergedBlocks[`${i + 4}`]) {
 					if (gameCells[i + 4].textContent === gameCells[i + 8].textContent) {
 						gameCells[i + 8].textContent = Number(gameCells[i + 4].textContent) * 2;
 						gameCells[i + 4].textContent = '';
+						mergedBlocks[`${i + 8}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 8].textContent === '' && gameCells[i + 4].textContent !== '') {
@@ -108,10 +115,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i + 4].textContent !== '') {
+				if (gameCells[i + 4].textContent !== '' && !mergedBlocks[`${i + 4}`]) {
 					if (gameCells[i].textContent === gameCells[i + 4].textContent) {
 						gameCells[i + 4].textContent = Number(gameCells[i].textContent) * 2;
 						gameCells[i].textContent = '';
+						mergedBlocks[`${i + 4}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 4].textContent === '' && gameCells[i].textContent !== '') {
@@ -119,13 +127,12 @@ function handleKeyDown(event) {
 					gameCells[i].textContent = '';
 					changed = true;
 				}
-
 			}
 		}
 
 		if (changed) {
-			freeCells = gameCells.filter(cell => cell.textContent === '')
-			toFillCell()
+			freeCells = gameCells.filter(cell => cell.textContent === '');
+			toFillCell();
 		}
 
 	}
@@ -136,11 +143,13 @@ function handleKeyDown(event) {
 		for (let j = 0; j < 4; j++) {
 
 			for (let i = 3; i < 16; i += 4) {
+				// debugger
 
-				if (gameCells[i - 3].textContent !== '') {
+				if (gameCells[i - 3].textContent !== '' && !mergedBlocks[`${i - 3}`] && !mergedBlocks[`${i - 2}`]) {
 					if (gameCells[i - 2].textContent === gameCells[i - 3].textContent) {
 						gameCells[i - 3].textContent = Number(gameCells[i - 2].textContent) * 2;
 						gameCells[i - 2].textContent = '';
+						mergedBlocks[`${i - 3}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i - 3].textContent === '' && gameCells[i - 2].textContent !== '') {
@@ -149,12 +158,12 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i - 2].textContent !== '') {
+				if (gameCells[i - 2].textContent !== '' && !mergedBlocks[`${i - 2}`] && !mergedBlocks[`${i - 1}`]) {
 					if (gameCells[i - 1].textContent === gameCells[i - 2].textContent) {
 						gameCells[i - 2].textContent = Number(gameCells[i - 1].textContent) * 2;
 						gameCells[i - 1].textContent = '';
+						mergedBlocks[`${i - 2}`] = true;
 						changed = true;
-						wasFolded = true;
 					}
 				} else if (gameCells[i - 2].textContent === '' && gameCells[i - 1].textContent !== '') {
 					gameCells[i - 2].textContent = gameCells[i - 1].textContent;
@@ -162,12 +171,12 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i - 1].textContent !== '') {
+				if (gameCells[i - 1].textContent !== '' && !mergedBlocks[`${i - 1}`]) {
 					if (gameCells[i].textContent === gameCells[i - 1].textContent) {
 						gameCells[i - 1].textContent = Number(gameCells[i].textContent) * 2;
 						gameCells[i].textContent = '';
+						mergedBlocks[`${i - 1}`] = true;
 						changed = true;
-						wasFolded = true;
 					}
 				} else if (gameCells[i - 1].textContent === '' && gameCells[i].textContent !== '') {
 					gameCells[i - 1].textContent = gameCells[i].textContent;
@@ -181,19 +190,20 @@ function handleKeyDown(event) {
 			freeCells = gameCells.filter(cell => cell.textContent === '');
 			toFillCell();
 		}
-
 	}
 
 
 	if (key === 'd' || key === 'ArrowRight') {
 
 		for (let j = 0; j < 4; j++) {
-			for (let i = 0; i < 13; i += 4) {
 
-				if (gameCells[i + 3].textContent !== '') {
+			for (let i = 0; i < 13; i += 4) {
+				// debugger
+				if (gameCells[i + 3].textContent !== '' && !mergedBlocks[`${i + 3}`] && !mergedBlocks[`${i + 2}`]) {
 					if (gameCells[i + 2].textContent === gameCells[i + 3].textContent) {
 						gameCells[i + 3].textContent = Number(gameCells[i + 2].textContent) * 2;
 						gameCells[i + 2].textContent = '';
+						mergedBlocks[`${i + 3}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 3].textContent === '' && gameCells[i + 2].textContent !== '') {
@@ -202,10 +212,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i + 2].textContent !== '') {
+				if (gameCells[i + 2].textContent !== '' && !mergedBlocks[`${i + 2}`] && !mergedBlocks[`${i + 1}`]) {
 					if (gameCells[i + 1].textContent === gameCells[i + 2].textContent) {
 						gameCells[i + 2].textContent = Number(gameCells[i + 1].textContent) * 2;
 						gameCells[i + 1].textContent = '';
+						mergedBlocks[`${i + 2}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 2].textContent === '' && gameCells[i + 1].textContent !== '') {
@@ -214,10 +225,11 @@ function handleKeyDown(event) {
 					changed = true;
 				}
 
-				if (gameCells[i + 1].textContent !== '') {
+				if (gameCells[i + 1].textContent !== '' && !mergedBlocks[`${i + 1}`]) {
 					if (gameCells[i].textContent === gameCells[i + 1].textContent) {
 						gameCells[i + 1].textContent = Number(gameCells[i].textContent) * 2;
 						gameCells[i].textContent = '';
+						mergedBlocks[`${i + 1}`] = true;
 						changed = true;
 					}
 				} else if (gameCells[i + 1].textContent === '' && gameCells[i].textContent !== '') {
@@ -232,10 +244,12 @@ function handleKeyDown(event) {
 			freeCells = gameCells.filter(cell => cell.textContent === '');
 			toFillCell();
 		}
+
 	}
 
-	addClassesToCell()
+	addClassesToCell();
+	// console.log(mergedBlocks)
+	mergedBlocks = {}
 }
 
-document.addEventListener('keydown', handleKeyDown)
-// document.addEventListener('click', toFillCell)
+document.addEventListener('keydown', handleKeyDown);
